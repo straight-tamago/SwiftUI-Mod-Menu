2bcc71cd3a6de860937d91389e6162bfdcd8a73b
straight-tamago <121408851+straight-tamago@users.noreply.github.com>
2026-03-03 16:14:32 +0900

    Delete libs/internal_do_not_delete/swift/windowkit/WKAliases.swift

--- libs/internal_do_not_delete/swift/windowkit/WKAliases.swift
+++
@@ -1,9 +0,0 @@
-import SwiftUI
-
-public typealias WKFloatingButton<Label: View> = FloatingButton<Label>
-public typealias WKFloatingWindow<Actions: View, Content: View> = FloatingWindow<Actions, Content>
-public typealias WKFloatingOverlay<Content: View> = FloatingOverlay<Content>
-
-public typealias WKButton<Label: View> = FloatingButton<Label>
-public typealias WKWindow<Actions: View, Content: View> = FloatingWindow<Actions, Content>
-public typealias WKOverlay<Content: View> = FloatingOverlay<Content>
